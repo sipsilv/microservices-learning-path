3791b6f5262d0216867912bc82b799e6befd4ccb
Santhosh Jallu <santhoshjallu@gmail.com>
2026-03-27 16:07:06 +0530

    Add catalog service setup

---
+++ src/main/java/dev/silvercapes/catalogmicroservices/CatalogMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package dev.silvercapes.catalogmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/silvercapes/catalogmicroservices/CatalogMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.silvercapes.catalogmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
