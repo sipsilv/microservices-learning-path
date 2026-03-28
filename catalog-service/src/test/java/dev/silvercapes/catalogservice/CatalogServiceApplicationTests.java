4abd89ee10916cb0010c142480b2ac58dcf5c75f
Santhosh Jallu <santhoshjallu@gmail.com>
2026-03-28 17:13:59 +0530

    Add github ci pipeline

--- catalog-service/src/test/java/dev/silvercapes/catalogservice/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/dev/silvercapes/catalogservice/CatalogServiceApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class CatalogServiceApplicationTests extends AbstractIntegrationTests{
+class CatalogServiceApplicationTests {
 
     @Test
     void contextLoads() {
